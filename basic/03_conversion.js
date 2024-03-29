8bcb364b7db4a68c3ea48db522e4b8e581b0cf3e
PuneetDabral <86554356+PuneetDabral@users.noreply.github.com>
2024-03-29 18:20:31 +0000

    some more in conversion

--- basic/03_conversion.js
+++ basic/03_conversion.js
@@ -17,3 +17,23 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 // now its value is NaN
 console.log(valueInNumber)
+
+
+// 
+let value =2;
+let ngValue = -value;
+console.log(ngValue)
+
+let a='Puneet';
+let b = ' dabral'
+console.log(a+b);
+
+console.log('1'+1+3); //113
+console.log(2+'2'); //22
+console.log(2+2+'2') //42 compu left to right
+
+// try to us bracket 
+console.log((3+4)*5%3)
+
+
+// comparison 
