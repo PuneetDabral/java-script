09a185ccf5896b4c408adfe70d2e51f6c2066b5a
PuneetDabral <86554356+PuneetDabral@users.noreply.github.com>
2024-03-29 10:23:05 +0000

    basic varible dec

---
+++ lecture-1/varible.js
@@ -0,0 +1,9 @@
+const id = 1234; //you cant chnage it
+
+let name = 'Puneet';  //block scope
+
+var std = 10; //globle scope , changeble in whole pgrm
+
+rollN0 = 3; // dec this wey also
+
+let age; // undefined 
