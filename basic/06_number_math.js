add1403a00654bbeaa40167fcb3ee32bdd35cc00
PuneetDabral <86554356+PuneetDabral@users.noreply.github.com>
2024-03-30 19:03:10 +0000

    math or number

---
+++ basic/06_number_math.js
@@ -0,0 +1,32 @@
+// const num =300;
+// const num1 = new Number(200);
+// console.log(num1.toFixed(2)) //200.00
+
+// const hun = 10000;
+// console.log(hun.toLocaleString('en-IN')) //10,000
+
+// maths
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(0.4))
+// console.log(Math.round(0.6))
+
+// console.log(Math.ceil(0.4))
+// console.log(Math.floor(0.9))
+
+// console.log(Math.min(1,4,0,5));
+// console.log(Math.max(1,4,0,5));
+
+// most imp
+console.log(Math.random()) //it gives value blw o to 1
+console.log(Math.floor((Math.random()*10)+1))
+
+const min=10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max-min+1)+min))
+
+console.log(Math.floor(Math.random()*(6-0+1)))
+
+
