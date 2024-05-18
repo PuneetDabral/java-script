c96e0e8b295a21b8b8ea99d41bcf43f038e37dae
PuneetDabral <86554356+PuneetDabral@users.noreply.github.com>
2024-05-18 17:08:24 +0000

    cantro flow stat..

---
+++ cantrol_flow/first.js
@@ -0,0 +1,41 @@
+// if
+// if(con){}
+
+let bal =30;
+if(bal===30){console.log('up')}else{console.log('down')}
+
+bal===30?console.log('up'):console.log('down');
+
+console.log(bal===30?'up':'down')
+
+// if(){}elseif(){}else
+
+
+// if(a==a && b==b) both condition should be true
+
+// switch(){
+    // case a:
+    //     abc;
+    //     break;
+    // default;
+// }
+
+// some truthy and falsy value
+// let a='abc' truthy value if it is empty it is falsy value
+// if(a){
+//     console.log('yes')
+// }else{console.log('no')}
+
+
+// falsy value > false,0,-0,BigInt 0n,'',null , undefined,NaN 
+
+// truthy value
+// '0','false'," ",[],{}
+
+// this way we can check array and object 
+// if(Array.length===0)
+
+// if(Object.keys(obj1).length===0)
+
+// nullish  operator (??): null undefined it takes second value
+// let val=1??5;
